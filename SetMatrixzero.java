9616df0d5c4fdecb230ded42c28c365f8c60de69
Hradesh <hradeshkrishanan@gmail.com>
2026-01-13 18:10:18 +0530

    Committed Brute force of set matrix zero

---
+++ SetMatrixzero.java
@@ -0,0 +1,49 @@
+class SetMatrixzero{
+
+    public static void setmatrix(int matrix[][]){
+        //m=row and n= column
+        int m=matrix.length;
+        int n=matrix[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    for(int col=0;col<n;col++){
+                        if(matrix[i][col]!=0){
+                            matrix[i][col]=-1;
+
+                        }
+                    }
+                    for(int row=0;row<m;row++){
+                        if(matrix[row][j]!=0){
+                            matrix[row][j]=-1;
+
+                        }
+                    }
+           
+                }   
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==-1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+
+    }
+    public static void main(String[]args){
+        int matrix[][]={{1,1,1,0},{1,0,1,0},{1,1,1,1}};
+        setmatrix(matrix);
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+        
+        
+        
+    }
+}
