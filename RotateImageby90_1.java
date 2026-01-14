df8fcbdf518920d46e4c8dc84058643a3346d2eb
Hradesh <hradeshkrishanan@gmail.com>
2026-01-14 19:57:47 +0530

    Optimal Approach of Rotate

---
+++ RotateImageby90_1.java
@@ -0,0 +1,47 @@
+class RotateImageby90_1{
+    public void Rotate(int matrix[][]){
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+
+            }
+
+        }
+        for(int i=0;i<n;i++){
+            int left=0;
+            int right=n-1;
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
+
+    }
+
+
+
+
+    public static void main(String[]args){
+        int matrix[][]={{1,2,3},{4,5,6},{7,8,9}};
+        RotateImageby90_1 obj=new RotateImageby90_1();
+        obj.Rotate(matrix);
+
+        // Print the rotated matrix
+        for (int[] row : matrix) {
+            for (int val : row){
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    
+        
+        
+        
+    }
+}
