3928eaed43c0abfc2eaf54d127a0ec756e83fd87
Hradesh <hradeshkrishanan@gmail.com>
2026-01-17 23:48:42 +0530

    Modified

--- MajorityElement0.java
+++ MajorityElement0.java
@@ -1,37 +1,61 @@
+import java.util.*;
 class MajorityElement0{
-    public static int Majority0(int arr[],int n){
-        int count=0;
-        int el=arr[0];
-        for(int i=0;i<n;i++){
-            if(count==0){
-                count=1;
-                el=arr[i];
+    public static List Majority0(int arr[],int n){
+        List<Integer> ans=new ArrayList<>();
 
-            }else if(el==arr[i]){
-                count++;
-
-            }else{
-                count--;
-            }
-
-
-
-        }
         int count1=0;
+        int count2=0;
+        int el1=Integer.MIN_VALUE;
+        int el2=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-            if(arr[i]==el){
-                count1++;
+            if(count1==0 && el1!=arr[i]){
+                count1=1;
+                el1=arr[i];
+
             }
-            if(count1>n/3){
-                return el;
+            else if(count2==0 && el2!=arr[i]){
+                count2=1;
+                el2=arr[i];
+            }
+            else if(el1==arr[i]){
+                count1++;
+
+            }else if(el2==arr[i]){
+                count2++;
+            }else{
+                count1--;
+                count2--;
+            }
+            
+        }
+        int min=n/3+1;
+        int count3=0;
+        int count4=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==el1){
+                count3++;
+                
+            }
+            if(arr[i]==el2){
+                count4++;
             }
         }
-        return -1;
+
+            if(count3>=min){
+                ans.add(el1);
+            }
+            if(count4>=min && el1!=el2){
+                ans.add(el2);
+            }
+        
+        return ans;
     }
     public static void main(String[]args){
-        int arr[]={3,2,2,1,1,1,3,2,2};
-        int ans=Majority0(arr,arr.length);
-        System.out.println(ans);
+        int arr[]={1,1,1,2,2,2};
+        List<Integer> result=Majority0(arr,arr.length);
+        for(int num:result){
+            System.out.print(num+" ");
+        }
 
     }
 }
