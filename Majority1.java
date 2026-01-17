a6b66d13e047d2e0ad43ef4f9fde5e0d11a3e179
Hradesh <hradeshkrishanan@gmail.com>
2026-01-17 23:32:21 +0530

    Modified

--- Majority1.java
+++ Majority1.java
@@ -1,23 +1,29 @@
 import java.util.*;
 class Majority1{
-    public static int Majority1(int arr[],int n){
+    public static List Majority1(int arr[],int n){
         HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num:arr){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            if(entry.getValue()>n/3){
-                return entry.getKey();
+        List<Integer> result=new ArrayList<>();
+        int min=n/3+1;
+        for(int i=0;i<n;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        
+            if(map.get(arr[i])==min){
+                result.add(arr[i]);
             }
-
+            if(result.size()==2){
+                break;
+            }
         }
-        return -1;
+        return result;
+    
 
     }
     public static void main(String[]args){
-        int arr[]={3,2,2,1,1,1,3,2,2};
-        int ans=Majority1(arr,arr.length);
-        System.out.println(ans);
+        int arr[]={1,1,1,2,2,2};
+        List<Integer> ans=Majority1(arr,arr.length);
+        for(int num:ans){
+            System.out.print(num+" ");
+        }
 
     }
 }
